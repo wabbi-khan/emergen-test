1b4809fc17c891f74baacd51c045f58d16ab8bd2
wabbi-khan <kwahab789@gmail.com>
2023-02-18 16:32:53 +0500

    create screen and add data in json file

--- components/Screens/Clinically.jsx
+++ components/Screens/Clinically.jsx
@@ -1,23 +1,7 @@
 import React from 'react';
-// import Logo from '../../public/assets/logo.png';
-// import Image from 'next/image';
-// import css from '../../styles/Home.module.css';
-// import { AiOutlineArrowLeft } from 'react-icons/ai';
-// import IntroImg1 from '../../public/assets/introImg1.png';
-// import IntroImg2 from '../../public/assets/introImg2.png';
-// import { SCREEN_TYPES, useAppContext } from '../../context/AppContext';
 
-const Clinically = () => {
-  //   const { pageDataSets, pageMethods, pageState } = useAppContext();
-
-  return (
-    <section>
-      <div className="s600">
-        <h2>screen size should be less than 600px</h2>
-      </div>
-      <div>asd</div>
-    </section>
-  );
+const clinically = () => {
+  return <div>clinically</div>;
 };
 
-export default Clinically;
+export default clinically;
